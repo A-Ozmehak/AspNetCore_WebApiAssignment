e016cbf5b622056505237a7eeb418d301975586c
Anna <nakupanda89@gmail.com>
2024-04-05 01:43:50 +0200

    changed subscriberEntity and added a dto

---
+++ Infrastructure/Dtos/SubscriberDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Dtos;
+
+public class SubscriberDto
+{
+    [Required]
+    public string Email { get; set; } = null!;
+    public bool DailyNewsletter { get; set; }
+    public bool AdvertisingUpdates { get; set; }
+    public bool WeekInReview { get; set; }
+    public bool EventUpdates { get; set; }
+    public bool StartupsWeekly { get; set; }
+    public bool Podcasts { get; set; }
+}
--- Infrastructure/Entities/SubscriberEntity.cs
+++ Infrastructure/Entities/SubscriberEntity.cs
@@ -1,7 +1,34 @@
-﻿namespace Infrastructure.Entities;
+﻿using Infrastructure.Dtos;
+using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Entities;
 
 public class SubscriberEntity
 {
-    public int Id { get; set; }
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    [Required]
     public string Email { get; set; } = null!;
+
+    public bool DailyNewsletter { get; set; }
+    public bool AdvertisingUpdates { get; set; }
+    public bool WeekInReview { get; set; }
+    public bool EventUpdates { get; set; }
+    public bool StartupsWeekly { get; set; }
+    public bool Podcasts { get; set; }
+
+
+    public static implicit operator SubscriberEntity(SubscriberDto dto)
+    {
+        return new SubscriberEntity
+        {
+            Email = dto.Email,
+            DailyNewsletter = dto.DailyNewsletter,
+            AdvertisingUpdates = dto.AdvertisingUpdates,
+            WeekInReview = dto.WeekInReview,
+            EventUpdates = dto.EventUpdates,
+            StartupsWeekly = dto.StartupsWeekly,
+            Podcasts = dto.Podcasts,
+        };
+    }
 }
