a14a27f09ce286b7ed1306db35d4ca892241c4d0
Anna <nakupanda89@gmail.com>
2024-04-17 12:49:16 +0200

    added accesstoken

---
+++ WebApi/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using Infrastructure.Contexts;
+using Infrastructure.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebApi.Filters;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController(DataContext dataContext, IConfiguration configuration) : ControllerBase
+{
+    private readonly DataContext _dataContext = dataContext;
+    private readonly IConfiguration _configuration = configuration;
+
+    [UseApiKey]
+    [HttpPost]
+    [Route("token")]
+    public IActionResult GetToken(UserLoginForm form)
+    {
+        if (ModelState.IsValid)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_configuration["Jwt:Secret"]!);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new(ClaimTypes.Email, form.Email),
+                    new(ClaimTypes.Name, form.Email),
+                }),
+                Expires = DateTime.UtcNow.AddDays(1),
+                Issuer = _configuration["Jwt:Issuer"],
+                Audience = _configuration["Jwt:Audience"],
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(tokenString);
+        }
+
+        return Unauthorized();
+    }
+}
--- WebApi/Filters/UseApiKeyAttribute.cs
+++ WebApi/Filters/UseApiKeyAttribute.cs
@@ -26,3 +26,24 @@ public class UseApiKeyAttribute : Attribute, IAsyncActionFilter
         await next();
     }
 }
+
+//[AttributeUsage(AttributeTargets.All)]
+//public class UseApiKeyAttribute : Attribute, IAsyncActionFilter
+//{
+//    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+//    {
+//        var configuration = context.HttpContext.RequestServices.GetService<IConfiguration>();
+//        var secret = configuration?["ApiKey:Secret"];
+
+//        if (!string.IsNullOrEmpty(secret) && context.HttpContext.Request.Query.TryGetValue("key", out var key))
+//        {
+//            if (!string.IsNullOrEmpty(key) && secret == key)
+//            {
+//                await next();
+//                return;
+//            }
+//        }
+
+//        context.Result = new UnauthorizedResult();
+//    }
+//}
